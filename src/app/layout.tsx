8af16cb07716942222ea0cda97a194df6b2ee881
Alexandra Sandoval <alexandoval13@gmail.com>
2025-02-06 14:00:49 -0800

    Adds font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Kirang_Haerang } from 'next/font/google';
 import './globals.css';
 
 // TODO: Fonts
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const kirang = Kirang_Haerang({
+  weight: '400',
   subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const geistSans = Geist({
+  variable: '--font-geist-sans',
   subsets: ['latin'],
 });
 
@@ -25,9 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${kirang} ${geistSans.variable}`}>{children}</body>
     </html>
   );
 }
