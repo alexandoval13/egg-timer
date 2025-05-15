390e51c1a1c5179b555a55f8e7bd7d4c24e449d2
Alexandra Sandoval <alexandoval13@gmail.com>
2025-05-15 10:55:32 -0700

    Abstract EggTimer page

--- src/app/eggs/EggContainer.tsx
+++ src/app/eggs/EggContainer.tsx
@@ -1,63 +1,35 @@
 'use client';
 import { useState } from 'react';
-import Timer from '../components/Timer';
 import EggCard from './EggCard';
 import Button from '../components/Button';
 import { EggType } from '@/types/Egg';
-import ChickenFaceLeftIcon from '../../../public/chicken-face-left-icon';
-import LeftArrowIcon from '../../../public/left-arrow-icon';
 
 import styles from './eggcontainer.module.css';
+import EggTimer from './EggTimer';
 
 type EggContainerProps = {
   data: EggType[];
 };
 
 export default function EggContainer({ data }: EggContainerProps) {
-  const [timer, setTimer] = useState<boolean>(false);
   const [egg, setEgg] = useState<EggType | null>(null);
 
-  const alert = new Audio('/audio-timer-alert.mp3');
-
   const handleStartEggTimer = (selectedEgg: EggType) => {
-    setTimer(true);
     setEgg(selectedEgg);
-  };
-
-  const handleComplete = () => {
-    alert.play();
   };
 
   const clearTimer = () => {
     setEgg(null); // Reset after completion
-    setTimer(false);
   };
 
-  const handleReturn = () => {
-    alert.pause();
+  const handleClear = () => {
     clearTimer();
   };
 
   return (
     <>
-      {timer && egg ? (
-        <div className={styles.container}>
-          <div className={styles['back-container']} onClick={handleReturn}>
-            <LeftArrowIcon height="80px" width="80px" />
-            <div className={styles.bounce}>
-              <ChickenFaceLeftIcon />
-            </div>
-          </div>
-          <div className={styles.card}>
-            <EggCard data={egg} />
-            <Timer
-              milliseconds={egg.time}
-              onComplete={handleComplete}
-              pauseEnabled
-              playAlert
-            />
-          </div>
-        </div>
+      {egg ? (
+        <EggTimer handleClear={handleClear} egg={egg} />
       ) : (
         <div className={styles['cards-container']}>
           {data.map((egg: EggType, i) => {
---
+++ src/app/eggs/EggTimer.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import Timer from '../components/Timer';
+import EggCard from './EggCard';
+import { EggType } from '@/types/Egg';
+import ChickenFaceLeftIcon from '../../../public/chicken-face-left-icon';
+import LeftArrowIcon from '../../../public/left-arrow-icon';
+
+import styles from './eggcontainer.module.css';
+
+type EggTimerProps = {
+  handleClear: () => void;
+  egg: EggType;
+};
+
+export default function EggTimer(props: EggTimerProps) {
+  const { handleClear, egg } = props;
+
+  const alert = new Audio('/audio-timer-alert.mp3');
+
+  const handleComplete = () => {
+    alert.play();
+  };
+
+  const handleReturn = () => {
+    handleClear();
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles['back-container']} onClick={handleReturn}>
+        <LeftArrowIcon height="80px" width="80px" />
+        <div className={styles.bounce}>
+          <ChickenFaceLeftIcon />
+        </div>
+      </div>
+      <div className={styles.card}>
+        <EggCard data={egg} />
+        <Timer
+          milliseconds={egg.time}
+          onComplete={handleComplete}
+          pauseEnabled
+        />
+      </div>
+    </div>
+  );
+}
