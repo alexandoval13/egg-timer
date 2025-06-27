ab8b9d628ef03c9314c38d2718dd5c004a0307b6
Alexandra Sandoval <alexandoval13@gmail.com>
2025-06-27 15:17:08 -0700

    handle hrs formatting

--- src/lib/formatTimeToString.ts
+++ src/lib/formatTimeToString.ts
@@ -1,10 +1,13 @@
 export const formatTimeToString = (ms: number): string => {
   const totalSeconds = Math.floor(ms / 1000);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = (totalSeconds % 3600) % 60;
 
   // Add leading zero to seconds if needed
   const secondsString = seconds < 10 ? '0' + seconds : seconds;
+  const minutesString = hours && minutes < 10 ? '0' + minutes : minutes;
+  const hoursString = hours ? hours + ':' : '';
 
-  return `${minutes}:${secondsString}`;
+  return `${hoursString}${minutesString}:${secondsString}`;
 };
